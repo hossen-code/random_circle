cb1b850b51efef10acb80b541b2ee75708ac6a9d
Hossein Ghayoor <hosseing@stradigi.ai>
2019-12-16 09:58:06 -0500

    changes regarding calculating average distance

--- rve_gen.py
+++ rve_gen.py
@@ -27,7 +27,10 @@ class Point: # not sure if classes need docstring
         self.coordinates = np.random.rand(1, 2) * np.array([WIDTH, HEIGHT])
         
     def move(self):
-        
+        # for moving the most secluded ones, we need to create another point in a circle
+        # with radius of the sum_average and check if it's colliding
+        # the other thing we can do not search the entire area to add the points, just around
+        # lastly the mirroring points in the edge
         move_val = np.random.rand(1, 2)* np.array([3, 3]) # 3 is the move intensity
         self.coordinates += move_val
     
@@ -73,19 +76,41 @@ def calculate_all_distances(points):
             
     return all_distances
 
-def sum_n_closest_point_distance(n_points: int = 3, distance_arrays):
+
+def sum_n_closest_point_distance(distance_arrays, n_points: int = 3):
     """
     based on the result of this we find the secluded points to move them
     towards their neighbors
     """
-    sum_n_close_points = []
+    sum_distance_n_close_points = np.array([])
     for dist_array in distance_arrays:
-        close_point_ind = np.argpartition(distance_arrays, n_points)
-        distance_sum = sum(distance_arrays[close_point_ind[:n_points]])
-        sum_n_close_points.append(distance_sum)
+        close_point_ind = np.argpartition(dist_array, n_points)
+        distance_sum = sum(dist_array[close_point_ind[:n_points]])
+        sum_distance_n_close_points = np.append(sum_distance_n_close_points,
+                                                distance_sum)
 
-    return sum_n_close_points
-    
+    return sum_distance_n_close_points
+
+
+def indices_of_most_secluded(sum_distance_n_close_points,
+                             n_moving_candidate=5,
+                             n_points = 3):
+    # this is not sorted (not biggest to smallest)
+    # returns index of n_moving_candidates
+    indices = np.argpartition(
+        sum_distance_n_close_points, -n_moving_candidate)[-n_moving_candidate:]
+    avg_empty_radius = np.average(sum_distance_n_close_points[indices])
+
+    return indices, avg_empty_radius
+
+
+def potentially_moving_points(point_collection,
+                              moving_candidate_indices,
+                              sum_distance_radius,
+                              n_moving_candidate):
+    pass
+
+
 if __name__ == "__main__":
     point_collection = []
     for i in range(100):
@@ -94,12 +119,11 @@ if __name__ == "__main__":
             point_collection.append(new_point)
 
     all_dist = calculate_all_distances(point_collection)
+    sum_three_closest_distance = sum_n_closest_point_distance(
+        all_dist, n_points=3)
+
     pass
 #    for point in points:
 #        point.move()
 
     show_points(point_collection)
-    
-
-    
-
